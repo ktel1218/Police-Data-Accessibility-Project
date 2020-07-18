7e5d8ef749e683e301e19fbdb8ac6bd3d216f060
Kim Thompson <bykimthompson@gmail.com>
2020-07-18 13:32:22 -0500

    added eslint, prettier, and relevant commands to the package.json. Also added an option to run this app in pm2 in production, for increased stability

--- utils/slackbots/suggestion-bot/index.js
+++ utils/slackbots/suggestion-bot/index.js
@@ -14,109 +14,127 @@ const CHANNEL_ID = 'C013XMN8RL3';
 
  */
 exports.handler = async (event) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(event, null, 2));
 
-    let body;
-    let statusCode = '200';
-    const headers = {
-        'Content-Type': 'application/json',
-    };
+  let body;
+  let statusCode = '200';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
-    try {
-        switch (event.httpMethod) {
-            case 'POST': {
-                console.log("hit the event endpoint");
-                let eventBody = JSON.parse(event.body);
-                console.log("switch case on type");
-                console.log("type: " + eventBody.type);
+  try {
+    switch (event.httpMethod) {
+      case 'POST':
+        {
+          console.log('hit the event endpoint');
+          let eventBody = JSON.parse(event.body);
+          console.log('switch case on type');
+          console.log('type: ' + eventBody.type);
 
-                switch (eventBody.type) {
-
-                  case 'url_verification': {
-                    // verify Events API endpoint by returning challenge if present
-                    console.log("sending back the challenge");
-                    body = { "challenge": eventBody.challenge };
-                    break;
-                  }
-
-                  case 'event_callback': {
-                    // Verify the signing secret
-                    if (!signature.isVerified(event)) {
-                      console.log("failed to verify signature");
-                      throw new Error("Unverified signature. Does this message have required metadata from Slack? Have secrets been exported?");
-                    }
-                    console.log("Request is verified");
-                    const {type, user, channel, text, bot_id, ts, client_msg_id, thread_ts} = eventBody.event;
-
-                    if(type === 'message') {
-
-                      if(channel == CHANNEL_ID && typeof bot_id == 'undefined' && typeof client_msg_id !== 'undefined'
-                        && (typeof thread_ts == 'undefined' || thread_ts == ts)) {
-                        // this isn't a message from a bot and it isn't a meta-message from slack
-                        console.log(text);
-                        let result = await message.send(channel, text, ts);
-                        console.log(result);
-                      }
-                    }
-                    else if (type === 'app_home_opened'){
-                      const args = {
-                        "token": process.env.SLACK_BOT_TOKEN,
-                        "user_id": user,
-                        "view": JSON.stringify({
-                             "type":"home",
-                                "blocks": [
-                                    {
-                                        "type": "section",
-                                        "text": {
-                                            "type": "mrkdwn",
-                                            "text": ":wave: This app gives suggestions on which channels to join based on volunteers' introductions. It's just a simple keyword match that won't persist any data about you."
-                                        }
-                                    },
-                                    {
-                                        "type": "divider"
-                                    },
-                                    {
-                                        "type": "section",
-                                        "text": {
-                                            "type": "mrkdwn",
-                                            "text": "If you'd like to contribute, you can find the code here. Feel free to make a PR, but you'll have to reach out to @Katie since she's hosting it."
-                                        }
-                                    },
-                                    {
-                                        "type": "context",
-                                        "elements": [
-                                            {
-                                                "type": "plain_text",
-                                                "text": "Author: @Katie\nAdmin: @Zach - Slack Admin "
-                                            }
-                                        ]
-                                    }
-                                ]
-                          })
-                      };
-                      let result = await axios.post(`${API_URL}/views.publish`, qs.stringify(args));
-                      console.log(result);
-                    }
-                    break;
-                  }
-                  default:
-                    throw new Error(`Unsupported type "${eventBody.type}"`);
-                }
+          switch (eventBody.type) {
+            case 'url_verification': {
+              // verify Events API endpoint by returning challenge if present
+              console.log('sending back the challenge');
+              body = { challenge: eventBody.challenge };
+              break;
             }
-                break;
-            default:
-                throw new Error(`Unsupported method "${event.httpMethod}"`);
-        }
-    } catch (err) {
-        statusCode = '400';
-        body = err.message;
-    } finally {
-        body = JSON.stringify(body);
-    }
 
-    return {
-        statusCode,
-        body,
-        headers,
-    };
+            case 'event_callback': {
+              // Verify the signing secret
+              if (!signature.isVerified(event)) {
+                console.log('failed to verify signature');
+                throw new Error(
+                  'Unverified signature. Does this message have required metadata from Slack? Have secrets been exported?'
+                );
+              }
+              console.log('Request is verified');
+              const {
+                type,
+                user,
+                channel,
+                text,
+                bot_id,
+                ts,
+                client_msg_id,
+                thread_ts,
+              } = eventBody.event;
+
+              if (type === 'message') {
+                if (
+                  channel == CHANNEL_ID &&
+                  typeof bot_id == 'undefined' &&
+                  typeof client_msg_id !== 'undefined' &&
+                  (typeof thread_ts == 'undefined' || thread_ts == ts)
+                ) {
+                  // this isn't a message from a bot and it isn't a meta-message from slack
+                  console.log(text);
+                  let result = await message.send(channel, text, ts);
+                  console.log(result);
+                }
+              } else if (type === 'app_home_opened') {
+                const args = {
+                  token: process.env.SLACK_BOT_TOKEN,
+                  user_id: user,
+                  view: JSON.stringify({
+                    type: 'home',
+                    blocks: [
+                      {
+                        type: 'section',
+                        text: {
+                          type: 'mrkdwn',
+                          text:
+                            ":wave: This app gives suggestions on which channels to join based on volunteers' introductions. It's just a simple keyword match that won't persist any data about you.",
+                        },
+                      },
+                      {
+                        type: 'divider',
+                      },
+                      {
+                        type: 'section',
+                        text: {
+                          type: 'mrkdwn',
+                          text:
+                            "If you'd like to contribute, you can find the code here. Feel free to make a PR, but you'll have to reach out to @Katie since she's hosting it.",
+                        },
+                      },
+                      {
+                        type: 'context',
+                        elements: [
+                          {
+                            type: 'plain_text',
+                            text: 'Author: @Katie\nAdmin: @Zach - Slack Admin ',
+                          },
+                        ],
+                      },
+                    ],
+                  }),
+                };
+                let result = await axios.post(
+                  `${API_URL}/views.publish`,
+                  qs.stringify(args)
+                );
+                console.log(result);
+              }
+              break;
+            }
+            default:
+              throw new Error(`Unsupported type "${eventBody.type}"`);
+          }
+        }
+        break;
+      default:
+        throw new Error(`Unsupported method "${event.httpMethod}"`);
+    }
+  } catch (err) {
+    statusCode = '400';
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 };
--- utils/slackbots/suggestion-bot/message.js
+++ utils/slackbots/suggestion-bot/message.js
@@ -1,4 +1,4 @@
-const axios = require('axios'); 
+const axios = require('axios');
 const qs = require('qs');
 const suggestions = require('./suggestions');
 
@@ -8,49 +8,48 @@ const API_URL = 'https://slack.com/api';
  *  Handling DM messages
  */
 
-
 /* Calling the chat.postMessage method to send a message */
 
-const send = async(channel, text, thread) => {
+const send = async (channel, text, thread) => {
   let message = '';
   let suggested = new Set();
   text = text.toLowerCase();
   for (let channelId in suggestions) {
     for (let i in suggestions[channelId]['keywords']) {
-      let keyword = (suggestions[channelId]['keywords'][i]);
+      let keyword = suggestions[channelId]['keywords'][i];
       if (text.includes(keyword.toLowerCase())) {
         suggested.add(channelId);
       }
     }
   }
   suggested = Array.from(suggested);
-  if (suggested.length > 0){
-    let suggestion_text = "• <#C014E2JGJAJ>\n";
+  if (suggested.length > 0) {
+    let suggestion_text = '• <#C014E2JGJAJ>\n';
     for (let i in suggested) {
       suggestion_text += `• <#${suggested[i]}>\n`;
     }
-    message = `:wave: Welcome! Based on your introduction, we have a few suggestions for rooms to join:\n ${suggestion_text}`
-  }
-  else {
-    message = `:wave: Welcome! We don't have any specific channel suggestions for you yet, so maybe start with <#C014E2JGJAJ>.`
+    message = `:wave: Welcome! Based on your introduction, we have a few suggestions for rooms to join:\n ${suggestion_text}`;
+  } else {
+    message = `:wave: Welcome! We don't have any specific channel suggestions for you yet, so maybe start with <#C014E2JGJAJ>.`;
   }
   const args = {
     token: process.env.SLACK_BOT_TOKEN,
     channel: channel,
     thread_ts: thread,
-    text: message
+    text: message,
   };
 
   let result;
 
   try {
-      result = await axios.post(`${API_URL}/chat.postMessage`, qs.stringify(args));
-      return result;
+    result = await axios.post(
+      `${API_URL}/chat.postMessage`,
+      qs.stringify(args)
+    );
+    return result;
   } catch (e) {
-
-      return e;
+    return e;
   }
 };
-
 
 module.exports = { send };
